9648a40c9f5f130d488c97fb74f65e7e7723d065
Adarshsharma22 <adarshsharma6222@gmail.com>
2026-05-27 07:20:33 +0200

    Update

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <section id="center">
+        <h1 className='text-4xl font-bold text-center'>Brain Training App</h1>
+        <p className='text-center text-lg mt-4'>Train your brain with fun and challenging games!</p>
+      </section>
+    </>
+  )
+}
+
+export default App
